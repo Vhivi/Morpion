a66f1cf636c5924748de4b804130483d97c6d186
Vhivi <david.angelelli@hotmail.fr>
2022-07-09 13:50:19 +0200

    Vérification des exigences PEP8

--- morpion.py
+++ morpion.py
@@ -16,55 +16,57 @@
 plateau = []
 for _ in range(0, 9):
     plateau.append(" ")
-    
+
+
 def afficherPlateau(plateau, joueur=None):
     print(" " + plateau[0] + " | " + plateau[1] + " | " + plateau[2] + " ")
     print("---|---|---")
     print(" " + plateau[3] + " | " + plateau[4] + " | " + plateau[5] + " ")
     print("---|---|---")
     print(" " + plateau[6] + " | " + plateau[7] + " | " + plateau[8] + " ")
-    
+
     if joueur:
         print("Le joueur {} a gagné !".format(joueur))
+
 
 def main():
     joueur = "X"
     tour = 0
-    
+
     while True:
         afficherPlateau(plateau)
         mouvement = int(input("Tour du joueur {}. Entrez un nombre de 1 à 9 :"
                               .format(joueur)))
-        
+
         # On soustraie 1 au mouvement car le tableau est indexé de 0 à 8
         mouvement -= 1
-        
+
         # On vérifie qu'on peut jouer dans la case demandée
         if plateau[mouvement] == " ":
             plateau[mouvement] = joueur
             tour += 1
-        else :
+        else:
             print("Le case est déjà occupée !")
             continue
-        
+
         # Vérification si les case sont pleines et du même symbole
         if plateau[0] == plateau[1] == plateau[2] != " " \
-        or plateau[3] == plateau[4] == plateau[5] != " " \
-        or plateau[6] == plateau[7] == plateau[8] != " " \
-        or plateau[0] == plateau[3] == plateau[6] != " " \
-        or plateau[1] == plateau[4] == plateau[7] != " " \
-        or plateau[2] == plateau[5] == plateau[8] != " " \
-        or plateau[0] == plateau[4] == plateau[8] != " " \
-        or plateau[2] == plateau[4] == plateau[6] != " " :
+                or plateau[3] == plateau[4] == plateau[5] != " " \
+                or plateau[6] == plateau[7] == plateau[8] != " " \
+                or plateau[0] == plateau[3] == plateau[6] != " " \
+                or plateau[1] == plateau[4] == plateau[7] != " " \
+                or plateau[2] == plateau[5] == plateau[8] != " " \
+                or plateau[0] == plateau[4] == plateau[8] != " " \
+                or plateau[2] == plateau[4] == plateau[6] != " ":
             afficherPlateau(plateau, joueur)
             break
-        
+
         # Si on arrive au tour 9, ça signifie que tout est rempli et qu'il n'y
         # a pas de gagnant.
         if tour == 9:
             print("Egalité")
             break
-        
+
         if joueur == "X":
             joueur = "O"
         else:
